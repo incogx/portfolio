406f1265698aab1064e401cc9c09d68daabede61
incogx <abdulsist23@gmail.com>
2025-12-10 14:52:45 +0530

    Update vite base and homepage

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,11 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+   base: '/portfolio/',    // <-- add this (exact repo name)
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
 });
+
+
